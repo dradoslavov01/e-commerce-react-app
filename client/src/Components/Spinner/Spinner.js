ee1f3358146180cf29c007c7a3019d8fbe34ae86
dradoslavov01 <daniradoslavov01@abv.bg>
2021-05-16 22:53:33 +0300

    Added Loader

--- client/src/Components/Spinner/Spinner.js
+++ client/src/Components/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import './Spinner.css';
 const Spinner = () => {
     return (
         <div className="spinner">
-            <div class="lds-ring">
+            <div className="lds-ring">
                 <div></div>
                 <div></div>
                 <div></div>
--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,4 +1,4 @@
-const url = 'heroku create my-cool-project';
+const url = 'https://ecommerce--react.herokuapp.com/';
 
 export const getAllItems = (category) => {
     const type = ((category && category !== 'undefined') ? `?category=${category}` : '')
@@ -6,26 +6,3 @@ export const getAllItems = (category) => {
         .then(res => res.json())
         .catch(err => alert(err))
 };
-
-function getOptions(method, body) {
-    const options = {
-        method,
-        headers: {}
-    };
-
-    const token = localStorage.getItem('authToken');
-    if (token != null) {
-        options.headers['X-Authorization'] = token;
-    };
-
-    if (body) {
-        options.headers['Content-Type'] = 'application/json';
-        options.body = JSON.stringify(body)
-    };
-
-    return options
-};
-
-export const login = (email, password) => {
-    const user = fetch()
-}
