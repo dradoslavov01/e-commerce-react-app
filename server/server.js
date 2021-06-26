3d91c928c05da54e1018a1397424a3013eeda670
dradoslavov01 <daniradoslavov01@abv.bg>
2021-06-26 15:14:54 +0300

    connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const db = require('./dbConnection');
 
 
 const app = express();
+db();
 
 app.use(cors());
 
@@ -11,6 +14,6 @@ app.get('/', (req, res) => {
 
 });
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.listen(port || 4000, () => console.log(`Server is listening on port ${port}`));
